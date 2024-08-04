e9bc379194db307cc5bc426de81c428a4e7af4da
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-04 20:55:09 +0200

    getComputerChoice function runs as expected

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+// generate a random number then use to determine computer move
+function getComputerChoice () {
+    let randomNumber = Math.random()
+    let computerMove = '';
+    // this function creates a random number and then creates a move for the computer based on that- 
+    // -random number. 3 Moves in total
+    if(randomNumber <= 0.33){
+        computerMove = 'paper'
+        return computerMove
+    }
+    else if (randomNumber >0.33 && randomNumber <= 0.66) {
+        computerMove = 'rock'
+        return computerMove}
+    else if (randomNumber > 0.66) {
+        computerMove = 'scissors'
+        return computerMove
+    }
+    
+    
+}console.log(getComputerChoice())
