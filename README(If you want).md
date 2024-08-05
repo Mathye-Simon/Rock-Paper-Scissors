3d3ddf06a3af3defb152ebc1e8ceade6c76d9ddd
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-05 22:16:26 +0200

    added playerMove = rock logic

--- README(If you want).md
+++ README(If you want).md
@@ -6,3 +6,6 @@ Ok so I have my index.html contains my webpage. Then I have created my assets fo
 First version I want to create is going to be played in the console. I will provide input which is my move out of 3 possible moves using the prompt function then get the computer to select a random move from the three moves. After getting these moves I compare using maybe a function. That function will take in two arguements my move and the computers. Ultimately I am comparing between 3 moves. Therefore the function must contain 3 conditions that will check if the moves are similar or different. If not similar then which 1 wins and which 1 loses. Then display the result. Seems easy to make. (Everything after this line is updates during or after finishing the project)
 
 Status:
+I am currently thinking of how to effectively check who wins and who loses.
+The first one I dealt with is the draw, which i just check if playerMove and computerMove are the same.
+Now, check if playermove = for example rock and computerMove = scissors player wins, playerScore += 1. Tested and it works so I will do the same logic for scissors and paper. One problem is that the score resets after each game
--- assets/logic.js
+++ assets/logic.js
@@ -17,13 +17,44 @@ function getComputerChoice () {
     }
 }
 
-console.log(getComputerChoice())
+
 
 // function promts user for input, that input becomes playermove
 function getHumanChoice (){
-    let input=prompt("What will be your move player?:  ")
+    let input = prompt("What will be your move player?:  ")
     let playerMove = input
     return playerMove
 }
 
-console.log (getHumanChoice())
+
+
+// decision of who wins and who loses
+function countScore (playerMove, computerMove) {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    let result = "";
+    // when moves are the same the score doesnt increase or decrease, (optional)
+    if (playerMove == computerMove) {
+        playerScore += 0;
+        computerScore += 0;
+        result = `Player move was: ${playerMove} and Computer move was: ${computerMove}. The result is: A Tie! The score for player is ${playerScore} and for computer is ${computerScore}`
+    }
+    // what happens when playerMove is = rock
+    else if (playerMove == "rock" && computerMove == "paper") {
+        computerScore += 1
+        result = `Player move was: ${playerMove} and Computer move was: ${computerMove}. The result is: computer wins! The score for player is ${playerScore} and for computer is ${computerScore}`
+        }
+
+    else if (playerMove == "rock" && computerMove == "scissors") {
+            playerScore += 1
+            result = `Player move was: ${playerMove} and Computer move was: ${computerMove}. The result is: Player wins! The score for player is ${playerScore} and for computer is ${computerScore}`
+        }
+    return result
+    }
+console.log(countScore(getHumanChoice(),getComputerChoice()))
+    //     else if (playerMove === "rock" && computerMove === "") {
+    //         playerMove += 1
+    //     }
+    //     else if (playerMove === "") {}
+    // }
